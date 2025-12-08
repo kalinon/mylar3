05488acc0b1fe83f130d99ec45c56d0a1109b392
Holden Omans <holden.omans@gmail.com>
2025-12-08 08:25:33 -0500

    fix some typos

--- mylar/webserve.py
+++ mylar/webserve.py
@@ -7781,7 +7781,7 @@ class WebInterface(object):
     def getComicArtwork(self, ComicID=None, imageURL=None):
 
         from mylar import cache
-        logger.info("Retrieving image for : " + comicID)
+        logger.info("Retrieving image for : " + ComicID)
         return cache.getArtwork(ComicID, imageURL)
 
     getComicArtwork.exposed = True
@@ -8021,8 +8021,10 @@ class WebInterface(object):
             if meta_data['cover_artist'] is not None:
                 issuecoverartist = meta_data['cover_artist']
             issuesummary = meta_data['summary']
+
+        iss_filename = None
+
         if meta_data is not None:
-            iss_filename = None
             if filelocation is not None:
                 iss_filename = os.path.split(filelocation)[1]
 
